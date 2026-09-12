3e83caf551e9ce0b8aeac59ed4eb140ed6c1ea54
Bailey Nguyen <bailey.tan.nguyen@gmail.com>
2026-09-12 13:25:16 -0500

    feat(formula): bump polyxml formula to v0.5.0

--- Formula/polyxml.rb
+++ Formula/polyxml.rb
@@ -1,8 +1,8 @@
 class Polyxml < Formula
   desc "High-performance, polyglot native XML data-binding engine"
   homepage "https://github.com/nth-bailey/PolyXML"
-  url "https://github.com/nth-bailey/PolyXML/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "916de950d8dcea22d9540e9f4f21047a8ba9ac7db083009d487783a35489ec2b"
+  url "https://github.com/nth-bailey/PolyXML/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "15f19e5f4f924da08fa71a7e47c7638db8bce43c75d54dee1a37b84e588eddda"
   license "MIT"
   head "https://github.com/nth-bailey/PolyXML.git", branch: "main"
 
